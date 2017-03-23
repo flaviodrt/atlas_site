382a09237ca4525702190030ed6163768ca4f80c
Flavio Duarte <flaviogabriel@gmail.com>
2017-03-22 22:59:20 -0300

    app de vereador criada

---
+++ councilman/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CouncilmanConfig(AppConfig):
+    name = 'councilman'
---
+++ councilman/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Councilman(models.Model):
+
+    name = models.CharField('Name', max_length=150)
+    page_link = models.CharField('Page Link', max_length=100)
+    phone_number = models.CharField('Phone Number', max_length=20)
+    email = models.CharField('E-mail', max_length=150)
+    address = models.CharField('Address', max_length=255)
+    floor = models.CharField('Floor', max_length=10)
+    room = models.CharField('Room', max_length=10)
+    biography = models.TextField('Biography')
+    party = models.CharField('Party', max_length=10)
+    picture = models.CharField('Picture', max_length=255)
