513f389b28e5c1c135743673330b783cca5296a2
Flavio Duarte <flaviogabriel@gmail.com>
2017-03-25 18:31:09 -0300

    página de detalhe do vereador

--- atlas/councilman/urls.py
+++ atlas/councilman/urls.py
@@ -3,6 +3,6 @@ from councilman import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^vereador/(?P<slug>[\w-]+)$', views.show, name='councilman'),
+    url(r'^vereador/(?P<slug>[\w-]+)$', views.detail, name='councilman'),
 
 ]
--- atlas/councilman/views.py
+++ atlas/councilman/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from councilman.models import Councilman
 
 
 def index(request):
     data = {
-        'councilman': Councilman.objects.all()
+        'councilmen': Councilman.objects.all().order_by('name')
     }
     return render(request, 'index.html', data)
 
 
-def show(request, slug):
-    pass
+def detail(request, slug):
+    data = {
+        'councilman': get_object_or_404(Councilman, slug=slug)
+    }
+    return render(request, 'detail.html', data)
