d85ea1e161218c6b8d6c92035275c2b4fdfd743c
Flavio Duarte <flaviogabriel@gmail.com>
2017-03-31 00:13:56 -0300

    configuração do projeto para usar postgresql

--- atlas/atlas/settings.py
+++ atlas/atlas/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'atlas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('database_name'),
+        'USER': config('database_user'),
+        'PASSWORD': config('database_password'),
+        'HOST': config('database_host'),
+        'PORT': '',
     }
 }
 
