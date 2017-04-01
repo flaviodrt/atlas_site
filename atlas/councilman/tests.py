98e5d8e58e1fe4223b2464d512b258f160e418ee
Flavio Duarte <flaviogabriel@gmail.com>
2017-04-01 11:39:43 -0300

    teste inicial

--- atlas/councilman/tests.py
+++ atlas/councilman/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url
 
 # Create your tests here.
+
+class TestGet(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(resolve_url('index'))
+
+    def test_http_ok(self):
+        self.assertEqual(200, self.response.status_code)
